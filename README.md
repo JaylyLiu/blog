5c6e89c9a5d78542c3d2f2294c3ff2de735411bb
Liu Jayly <jayly@italki.com>
2018-08-16 14:58:52 +0800

    add a Dashboard component as demo

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # 参考文档
 ## css使用说明
-在对应的文件中创建 fileName.scss 文件之后，将自动创建一个对应编译后的 fileName.css 文件，直接引用css文件进行使用
+在对应的文件中创建 FileName.scss 文件之后，将自动创建一个对应编译后的 FileName.css 文件，直接引用css文件进行使用
 
-> import from './fileName.css';<br/>
-> import styles from './fileName.css';
+> import from './FileName.css';<br/>
+> import styles from './FileName.css';
+
+* css/sass名一律和组件名保持一致
+* class类
--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
+import Dashboard from '../pages/Dashboard';
 
 class App extends Component {
+  state = {}
   render() {
+    let array1 = [
+      {
+        name: 'KG',
+        age: 23,
+        sex: 'male'
+      },
+      {
+        name: 'Jayly',
+        age: 18,
+        sex: 'female'
+      },
+      {
+        name: 'Gulu',
+        age: 1,
+        sex: 'male'
+      },
+      {
+        name: 'Zhiyi',
+        age: 1,
+        sex: 'female'
+      },
+      {
+        name: 'The little little little caty',
+        age: 0.1,
+        sex: 'male'
+      },
+    ]
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <div className="body">
+          <Dashboard
+            // 如果不传递string1 将使用默认值
+            // string1='STRING1'
+            string2='STRING2'
+            array1={array1}
+          />
+        </div>
       </div>
     );
   }
